ef75dc0a6524df1dbe8730ea04fef7aa66eda0f0
JulienF-Q <56581824+JulienF-Q@users.noreply.github.com>
2020-04-06 14:56:07 +0200

    Add files via upload

--- Page d'inscription/PageDInscription.php
+++ Page d'inscription/PageDInscription.php
@@ -67,38 +67,7 @@
     </form>
 </div>
 </body>
-<footer>
-
-
-    <span class="contact">
-        <h3>Nous contacter :</h3>
-    <p >
-        <img src="noun_Mail_3146799.png" class="mail"> <a href="mailto:datapta.officiel@gmail.com" id="mail">datapta.officiel@gmail.com</a>
-    </p>
-        </br>
-
-        <p>
-        <img src="telbis.png" class="phone">
-
-
-
-    </p>
-    </span>
-    <span class="mention">
-    <p >
-        Mention légales</br>
-        Condition générales d'utilisation
-    </p>
-    </span>
-    <span class="logo">
-        <img src="facebook-3-logo-png-transparent.png" id="facebook">
-
-        <img src="Linkedin_circle.svg.png" id="linkedin">
-
-        <img src="twitter.png" id="twitter">
-
-        <img src="instagram-Logo-PNG-Transparent-Background-download-1-1.png" id="instagram">
-        <p>© DatApta-Tous droits réservés</p>
-    </span>
-</footer>
+<?php
+require "footer.php";
+?>
 </html>
