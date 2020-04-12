af45ece0eecd5f434175db0578feb82d35c5ac0a
JulienF-Q <56581824+JulienF-Q@users.noreply.github.com>
2020-04-12 16:26:46 +0200

    Add files via upload

--- footer/footer.php
+++ footer/footer.php
@@ -1,16 +1,16 @@
 <html>
 
 <footer>
-    <link rel="stylesheet" href="footer.css">
+    <link rel="stylesheet" href="footer/footer.css">
     <span class="contact">
         <h3>Nous contacter :</h3>
     <p >
-        <img src="noun_Mail_3146799.png" class="mail"> <a href="mailto:datapta.officiel@gmail.com" id="mail">datapta.officiel@gmail.com</a>
+        <img src="Images/noun_Mail_3146799.png" class="mail"> <a href="mailto:datapta.officiel@gmail.com" id="mail">datapta.officiel@gmail.com</a>
     </p>
         </br>
 
         <p>
-        <img src="telbis.png" class="phone">
+        <img src="Images/telbis.png" class="phone">
 
 
 
@@ -23,13 +23,13 @@
     </p>
     </span>
     <span class="logo">
-        <img src="facebook-3-logo-png-transparent.png" id="facebook">
+        <img src="Images/facebook-3-logo-png-transparent.png" id="facebook">
 
-        <img src="Linkedin_circle.svg.png" id="linkedin">
+        <img src="Images/Linkedin_circle.svg.png" id="linkedin">
 
-        <img src="twitter.png" id="twitter">
+        <img src="Images/twitter.png" id="twitter">
 
-        <img src="instagram-Logo-PNG-Transparent-Background-download-1-1.png" id="instagram">
+        <img src="Images/instagram-Logo-PNG-Transparent-Background-download-1-1.png" id="instagram">
         <p>© DatApta-Tous droits réservés</p>
     </span>
 </footer>
