62cc6b7d35b62f227468c92a3377fd54806fa8a8
Lwii-U <58178996+Lwii-U@users.noreply.github.com>
2020-05-22 15:53:38 +0200

    Refonte

--- PageDeGestionDesTests/GestionTest.php
+++ PageDeGestionDesTests/GestionTest.php
@@ -1,71 +1,40 @@
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/html">
-<title>Accueil</title>
+<html lang="fr" xmlns="http://www.w3.org/1999/html">
 <head>
+    <meta charset="utf-8">
+    <title>Accueil</title>
+    <link rel="stylesheet" type="text/css" href="PageDeGestionDesTests\GestionTest.css">
+    <link rel="icon" type="image" href="Images\Infinite_measures_1.gif">
+</head>
 
-    <link rel="stylesheet" href="PageDeGestionDesTests/Gestiontest.css">
-
-
-    <head>
-        </p>
-        <ul>
-            <img src = 'Images/Infinite_measures_logo.png'width = "170"height="145">
-            <li><a class = "active" href="accueil">Accueil</a></li>
-            <li><a href="statistiques">Statistiques</a></li>
-            <li class="dropdown">
-                <a href="javascript:void(0)" class="dropbtn">Communauté</a>
-                <div class="dropdown-content">
-                    <a href="foire">FAQ</a>
-                    <a href="#">Classement</a>
-                    <a href="recherche">Recherche Utilisateur</a>
-                    <a href="mess">Messagerie</a>
-                </div>
-            </li>
-            <li><a href="profil">Mon profil</a></li>
-            <li class="dropdown">
-                <a href="javascript:void(0)" class="dropbtn">Gestion</a>
-                <div class="dropdown-content">
-                    <a href="gestion_u">Gestion Utilisateur</a>
-                    <a href="gestion_faq">Gestion FAQ</a>
-                    <a href="capteur">Gestion Capteurs</a>
-                </div>
-            </li>
-
-        </ul>
-    </head>
+<?php require "header\header.php" ?>
 
 <body>
-<p><img src = 'Images/aircraft-01-1254871.jpg'
-        width = 100%
-        height = "588">
-</p>
-<div id="BoxTest">
-    <form id="Ajouter" method="post" action="add_test">
-        <p>Ajouter :  Nom :<input type="text" name="Nom" > id des capteurs: <input type="text" name="Ids">
-            <input type="submit" value="Ajouter" name="submit">
-        </p>
-        </br>
+<div class="contentBlock" id="addBlock">
+    <form method="post" action="add_test">
+        <h3 id="titleOfAdd">Ajouter</h3>
+        <p><label for="nom">Nom : </label><input type="text" name="Nom" id="nom"></p>
+        <p><label for="idCapteurs">Id des capteurs : </label><input type="text" name="Ids" id="idCapteurs"></p>
+        <input type="submit" value="Ajouter" name="submit" id="add">
     </form>
+</div>
+<div id="listOfTests">
     <?php
     $test=selecttest();
     $nombre=count($test);
     for ($i=0;$i <$nombre;$i++){
+        echo ("<div class='contentBlock test'>");
         $ID=$test[$i]['id'];
-        echo ($test[$i]['Nom']);
-        echo "    ";
-        echo "Id des capteurs : ";
-        echo ($test[$i]['id capteurs']);
-        echo "  ";
-        echo("<a href='delete_test/$ID' class='option'>Supprimer</a>");
-        echo("</br>");
-        echo("</br>");
+        echo ("<h3>".$test[$i]['Nom']."</h3>");
+        echo ("<p>Id des capteurs : ".$test[$i]['id capteurs']."</p>");
+        echo("<a href='delete_test/$ID' class='delete'>Supprimer</a>");
+        echo ("</div>");
     }
     ?>
-
 </div>
 </body>
-<?php
-require "footer/footer.php"
-?>
+
+<?php require "footer/footer.php" ?>
+
 </html>
 
