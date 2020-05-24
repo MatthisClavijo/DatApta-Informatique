5b6a7af9487c8e4b85d22a6d75c03036e9c84246
annab_000 <gouin.annabelle98@gmail.com>
2020-05-24 19:32:03 +0200

    Update PageProfilEN +  RechercheUtilisateurEN

--- PageProfil/PageProfilEN.php
+++ PageProfil/PageProfilEN.php
@@ -1,82 +1,31 @@
 <!DOCTYPE html>
-<html>
-<title>Profile</title>
-
+<html lang="en">
 <head>
-    <link rel="stylesheet" href="PageProfil/PageProfil.css">
+    <meta charset="utf-8">
+    <title>My profile</title>
+    <link rel="stylesheet" type="text/css" href="PageProfil\PageProfil.css">
+    <link rel="icon" type="image" href="Images\Infinite_measures_1.gif">
+</head>
 
-
-    <head>
-        </p>
-        <ul>
-            <img src = 'Images/Infinite_measures_logo.png'width = "170"height="145">
-            <li><a class = "active" href="accueil">Home</a></li>
-            <li><a href="statistiques">Statistics</a></li>
-            <li class="dropdown">
-                <a href="javascript:void(0)" class="dropbtn">Community</a>
-                <div class="dropdown-content">
-                    <a href="foire">FAQ</a>
-                    <a href="#">Ranking</a>
-                    <a href="recherche">User search</a>
-                    <a href="mess">Mailbox</a>
-                </div>
-            </li>
-            <li><a href="profil">My profile</a></li>
-            <?php
-            if($_SESSION['type']=="admin"){
-                echo"  <li class=\"dropdown\">
-                <a href=\"javascript:void(0)\" class=\"dropbtn\">Admin</a>
-                <div class=\"dropdown-content\">
-                    <a href=\"gestion_u\">User manager</a>
-                    <a href=\"gestion_faq\">FAQ manager</a>
-                    <a href=\"capteur\">Sensors manager</a>
-                </div>";
-            }
-            ?>
-        </ul>
-    </head>
+<?php require "headerEN\header.php" ?>
 
 <body>
-<p><img src = 'Images/aircraft-01-1254871.jpg'
-        width = 100%
-        height = "588">
-</p>
-<form id="boxprofil">
-    <h3 id="title">Profile</h3>
-    <div id="ligne"></div>
-    <p>
-    <div id="name">
-        Name: <?php
-        echo ($_SESSION['nom']);
-        ?>
-    </div>
-    <div id="email">
-        Email address:  <?php
-        echo ($_SESSION['Adresse mail']);
-        ?>
-    </div>
-    </p>
-    <p>
-    <div id="prénom">
-        First name:  <?php
-        echo ($_SESSION['prénom']);
-        ?>
-    </div>
-
-    <div id="date">
-        Date of birth: <?php
-        echo ($_SESSION['date_de_naissance']);
-        ?>
-    </div>
-
-    </p>
-    <p>
-        <a id="modif" href="modifprofil">Modify profile</a>
-    </p>
-</form>
+<div class="contentBlock" id="profileBlock">
+    <h3 id="profileTitle">Profile</h3>
+    <?php
+    echo ("<div id='privateInfos'>");
+    echo ("<h4>Personal details: </h4>");
+    echo ("<p>Full name: ".$_SESSION['prénom']." ".$_SESSION['nom']."</p>");
+    echo ("<p>Date of birth: ".$_SESSION['date_de_naissance']."</p>");
+    echo ("</div>");
+    echo ("<div id='idInfos'>");
+    echo ("<h4>Login details: </h4>");
+    echo ("<p>Email address: ".$_SESSION['Adresse mail']."</p>");
+    echo ("</div>");
+    ?>
+    <p><a href="modifprofil" id="linkModifProfile">Modify my profile</a></p>
+</div>
 </body>
-<?php
-require "footerEN/footer.php";
-?>
-</html>
 
+<?php require "footerEN/footer.php"; ?>
+</html>
--- PageRechercheUtilisateur/RechercheUtilisateurEN.php
+++ PageRechercheUtilisateur/RechercheUtilisateurEN.php
@@ -1,104 +1,64 @@
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/html">
-<title>Users management</title>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
 <head>
+    <meta charset="utf-8">
+    <title>User search</title>
+    <link rel="stylesheet" type="text/css" href="PageRechercheUtilisateur\RechercheUtilisateur.css">
+    <link rel="icon" type="image" href="Images\Infinite_measures_1.gif">
+</head>
 
-    <link rel="stylesheet" href="PageRechercheUtilisateur/RechercheUtilisateur.css">
-
-
-    <head>
-        </p>
-        <ul>
-            <img src = 'Images/Infinite_measures_logo.png'width = "170"height="145">
-            <li><a class = "active" href="accueil">Home</a></li>
-            <li><a href="statistiques">Statistics</a></li>
-            <li class="dropdown">
-                <a href="javascript:void(0)" class="dropbtn">Community</a>
-                <div class="dropdown-content">
-                    <a href="foire">FAQ</a>
-                    <a href="#">Ranking</a>
-                    <a href="recherche">User search</a>
-                    <a href="mess">Mailbox</a>
-                </div>
-            </li>
-            <li><a href="profil">My profile</a></li>
-            <?php
-            if($_SESSION['type']=="admin"){
-                echo"  <li class=\"dropdown\">
-                <a href=\"javascript:void(0)\" class=\"dropbtn\">Admin</a>
-                <div class=\"dropdown-content\">
-                    <a href=\"gestion_u\">User manager</a>
-                    <a href=\"gestion_faq\">FAQ manager</a>
-                    <a href=\"capteur\">Sensors manager</a>
-                </div>";
-            }
-            ?>
-        </ul>
-    </head>
+<?php require "headerEN\header.php" ?>
 
 <body>
-<p><img src = 'Images/aircraft-01-1254871.jpg'
-        width = 100%
-        height = "588">
-</p>
-<div id="BoxUtilisateur">
+<div class="contentBlock" id="searchBlock">
     <form method="post" action="recherche_users">
-        <input type="text" placeholder="Enter a name" id="recherche" name="recherche">
-        <input type="radio" name="type" value="client">Client
-        <input type="radio" name="type" value="admin">Administrator
-        <input type="submit" value="Search" name="submit" id="bouton">
+        <p><label for="recherche">Search: </label><input type="text" placeholder="Enter a name" name="recherche"
+                                                              id="recherche"></p>
+        <p><label for="searchUser">Client</label><input type="radio" name="type" value="client" id="searchUser"></p>
+        <p><label for="searchAdmin">Administrator</label><input type="radio" name="type" value="admin"
+                                                                 id="searchAdmin"></p>
+        <input type="submit" value="Search" name="submit" id="searchButton">
     </form>
-    </br>
+</div>
 
+<div id="listOfRegistered">
     <?php
-    if ($_SESSION['search']=="vide") {
-        echo "<div id='titre1'>Client:</div> ";
-        echo "</br>";
+    if ($_SESSION['search'] == "vide") {
+        echo ("<div class='contentBlock' id='usersBlock'>");
+        echo ("<h3>Users</h3>");
         $utilisateur = selectuser();
         $nombre = count($utilisateur);
         for ($i = 0; $i < $nombre; $i++) {
             $ID = $utilisateur[$i]['ID'];
-            echo($utilisateur[$i]['nom']);
-            echo "  ";
-            echo($utilisateur[$i]['prénom']);
-            echo "  ";
-            echo("<a href='#' class='message'>Send a message</a>");
-            echo("</br>");
-            echo("</br>");
+            echo ("<p>".$utilisateur[$i]['prénom']." ".$utilisateur[$i]['nom']."    ".
+                "<a href='#' class='message'>Send a message</a></p>");
         }
-        echo "<div id='titre1'>Administrators:</div> ";
-        echo "</br>";
+        echo ("</div>");
+
+        echo ("<div class='contentBlock' id='adminsBlock'>");
+        echo ("<h3>Administrators</h3>");
         $administrateur = selectadmin();
         $nombre2 = count($administrateur);
         for ($i = 0; $i < $nombre2; $i++) {
             $ID = $administrateur[$i]['ID'];
-            echo($administrateur[$i]['nom']);
-            echo "  ";
-            echo($administrateur[$i]['prénom']);
-            echo "  ";
-            echo("<a href='#' class='message'>Send a message</a>");
-            echo("</br>");
-            echo("</br>");
-
+            echo("<p>".$administrateur[$i]['prénom']." ".$administrateur[$i]['nom']."    ".
+                "<a href='#' class='message'>Send a message</a></p>");
         }
-
+        echo ("</div>");
     }
-    if($_SESSION['search'] !="vide"){
-        echo ($_SESSION['search'][0]);
-        echo ("  ");
-        echo ($_SESSION['search'][1]);
+    if ($_SESSION['search'] != "vide") {
+        echo($_SESSION['search'][0]);
+        echo("  ");
+        echo($_SESSION['search'][1]);
         echo "  ";
         echo("<a href='#' class='message'>Send a message</a>");
         echo("</br>");
         echo("</br>");
-        $_SESSION['search']="vide";
+        $_SESSION['search'] = "vide";
     }
-
     ?>
-
 </div>
 </body>
-<?php
-require "footerEN/footer.php"
-?>
+
+<?php require "footerEN/footer.php" ?>
 </html>
