12f196f3da4a142ab1c90af1c7e34266d8b63294
JulienF-Q <56581824+JulienF-Q@users.noreply.github.com>
2020-05-29 18:58:25 +0200

    Add files via upload

--- PageStatistiques/PageStatistiques.php
+++ PageStatistiques/PageStatistiques.php
@@ -28,13 +28,22 @@
     </aside>
 
     <div id="mainContent">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque mattis sodales feugiat. Curabitur finibus nec ligula a volutpat. Nullam ut purus metus. Vivamus condimentum, dolor quis maximus suscipit, magna dolor scelerisque lectus, at congue dolor felis at nisi. Suspendisse ac turpis rhoncus, sodales nisl vel, laoreet nisl. Maecenas nec varius justo. Suspendisse luctus augue ut libero sagittis, sit amet efficitur erat fermentum.
+        <p>
+            <?php
+            $data = SelectResult($_SESSION['ID']);
+            for ($i=0; $i<sizeof($data);$i++) {
+                $data2=SelectUnit($data[$i][4]);
+                echo($data[$i][4]);
+                echo(" : ");
+                echo($data[$i][5]);
+                echo ("  ");
+                echo ($data2[0][0]);
+                echo ("</br>");
+                echo ("</br>");
+            }
+            ?>
 
-            Aenean quis tempor neque. Integer a pretium nunc, in volutpat purus. Donec justo massa, accumsan eget elit vitae, viverra sodales libero. Maecenas a vulputate mi. In lobortis gravida placerat. Aenean commodo porttitor varius. Proin suscipit dui vitae cursus convallis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam a arcu vel tortor suscipit lobortis.
-
-            Sed viverra ipsum sapien, in tempus felis gravida maximus. Phasellus sit amet ligula accumsan, posuere sapien vitae, imperdiet nunc. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque ut sagittis nunc. Nam vel purus magna. Sed vitae nibh vitae ex interdum mattis id at dui. Integer efficitur sit amet metus vel finibus. Fusce in placerat leo.
-
-            Morbi feugiat, elit ac pretium auctor, mauris ex scelerisque mi, accumsan dictum nulla eros eget nisi. Aliquam erat volutpat. Phasellus in eleifend magna. Sed et arcu ac enim sagittis ultricies nec non risus. Donec consectetur a velit nec dignissim. Sed vel orci aliquet, porttitor eros sed, aliquam nisl. Nulla placerat commodo accumsan. Sed nisi nisi, viverra eu luctus et, commodo tempor neque. Donec a porttitor ligula. Cras at justo eu risus hendrerit molestie non et felis. Nullam condimentum augue et venenatis malesuada. Nunc porta enim eu felis rhoncus facilisis.</p>
+        </p>
     </div>
 
 </body>
