759d2c5c8a457d336d9e939e21a20b97b01ffd3e
JulienF-Q <56581824+JulienF-Q@users.noreply.github.com>
2020-06-01 17:00:47 +0200

    Add files via upload

--- Messagerie/Messagerie.php
+++ Messagerie/Messagerie.php
@@ -7,43 +7,12 @@
 <head>
     <link rel="stylesheet" href="Messagerie/Messagerie.css">
 
+    <?php require "header/header.php";?>
 
-    <head>
-        </p>
-        <ul>
-            <img src = 'Images/Infinite_measures_logo.png'width = "170"height="145">
-            <li><a class = "active" href="accueil">Accueil</a></li>
-            <li><a href="statistiques">Statistiques</a></li>
-            <li class="dropdown">
-                <a href="javascript:void(0)" class="dropbtn">Communauté</a>
-                <div class="dropdown-content">
-                    <a href="foire">FAQ</a>
-                    <a href="#">Classement</a>
-                    <a href="recherche">Recherche Utilisateur</a>
-                    <a href="mess">Messagerie</a>
-                </div>
-            </li>
-            <li><a href="profil">Mon profil</a></li>
-            <?php
-            if($_SESSION['type']=="admin"){
-                echo"  <li class=\"dropdown\">
-                <a href=\"javascript:void(0)\" class=\"dropbtn\">Gestion</a>
-                <div class=\"dropdown-content\">
-                    <a href=\"gestion_u\">Gestion Utilisateur</a>
-                    <a href=\"gestion_faq\">Gestion FAQ</a>
-                    <a href=\"capteur\">Gestion Capteurs</a>
-                </div>";
-            }
-            ?>
-        </ul>
-    </head>
 </head>
 <body>
-<p><img src = 'Images/aircraft-01-1254871.jpg'
-        width = 100%
-        height = "588">
-</p>
-<div id="MessageBox">
+
+<p id="MessageBox">
     <?php
     $listeconv=getallconv($_SESSION['nom']);
     $user=$_SESSION["nom"];
@@ -77,7 +46,7 @@
     }
 
     ?>
-</div>
+</p>
 </body>
 <?php
 require "footer/footer.php"
