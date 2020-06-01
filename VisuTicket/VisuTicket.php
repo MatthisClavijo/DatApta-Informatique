f9bc0ea3914687894d028de27b77caaca2714321
JulienF-Q <56581824+JulienF-Q@users.noreply.github.com>
2020-06-01 17:10:03 +0200

    Add files via upload

--- VisuTicket/VisuTicket.php
+++ VisuTicket/VisuTicket.php
@@ -1,45 +1,12 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/html">
 <title>Accueil</title>
-<head>
 
     <link rel="stylesheet" href="VisuTicket/VisuTicket.css">
 
 
-    <head>
-        </p>
-        <ul>
-            <img src = 'Images/Infinite_measures_logo.png'width = "170"height="145">
-            <li><a class = "active" href="accueil">Accueil</a></li>
-            <li><a href="statistiques">Statistiques</a></li>
-            <li class="dropdown">
-                <a href="javascript:void(0)" class="dropbtn">Communauté</a>
-                <div class="dropdown-content">
-                    <a href="foire">FAQ</a>
-                    <a href="#">Classement</a>
-                    <a href="recherche">Recherche Utilisateur</a>
-                    <a href="mess">Messagerie</a>
-                    <a href="Ticket">Voir Ticket</a>
-                </div>
-            </li>
-            <li><a href="profil">Mon profil</a></li>
-            <li class="dropdown">
-                <a href="javascript:void(0)" class="dropbtn">Gestion</a>
-                <div class="dropdown-content">
-                    <a href="gestion_u">Gestion Utilisateur</a>
-                    <a href="gestion_faq">Gestion FAQ</a>
-                    <a href="capteur">Gestion Capteurs</a>
-                </div>
-            </li>
-        </ul>
-    </head>
-
-<body>
-<p><img src = 'Images/aircraft-01-1254871.jpg'
-        width = 100%
-        height = "588">
-</p>
-<div id="BoxTicket" >
+  <?php require "header/header.php"?>
+    <p id="BoxTicket">
     <?php
     $data=GetTicket();
     for ($i=0;$i<sizeof($data);$i++){
@@ -56,7 +23,7 @@
     }
 
     ?>
-</div>
+    </p>
 </body>
 <?php
 require "footer/footer.php"
