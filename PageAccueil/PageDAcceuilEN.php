47c0234cf672f7dde186b0f7ed41741bf61f3ef1
JulienF-Q <56581824+JulienF-Q@users.noreply.github.com>
2020-06-03 18:48:37 +0200

    Add files via upload

---
+++ PageAccueil/PageDAcceuilEN.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="PageAccueil/PageDAcceuil.css">
+    <link rel="icon" type="image" href="Images/Infinite_measures_1.gif">
+</head>
+
+<?php require "header/headerEN.php" ?>
+
+<body>
+<div id="vide"></div>
+</body>
+
+<?php require "footer/footerEN.php" ?>
+
+</html>
